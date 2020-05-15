1e550b39b204ffdb13f1d9d8c308e5702f222f29
unmeshdusane20 <unmeshdusane20@gmail.com>
2020-05-15 22:18:01 +0530

    handled click on cell and switch player@

--- src/components/Grids/Grids.vue
+++ src/components/Grids/Grids.vue
@@ -3,6 +3,7 @@
     <div class="grids">
       <div class="row" v-for="(row, rowKey, rowindex) in grid.matrix" :key="rowKey">
         <div class="cell"
+        @click="onClickCell($event, cell)"
         :class="{
           'empty': cell.value == 0,
           'hover-true': cell.hover,
@@ -13,8 +14,6 @@
       </div>
     </div>
     <div>{{selectedPlayer}}</div>
-
-
   </div>
 </template>
 <script>
@@ -51,6 +50,53 @@
             }
           }
         }
+      },
+
+      // check wiiing cell on click
+      checkWinner (item) {
+
+      },
+
+      togglePlayer () {
+        var _this = this
+        if(this.selectedPlayer.index) {
+          this.selectedPlayer = {
+            index: 0,
+            player: _this.players[0]
+          }
+        } else {
+          this.selectedPlayer = {
+            index: 1,
+            player: _this.players[1]
+          }
+        }
+      },
+
+
+      onClickCell(event, item) {
+        var column = [];
+        for(var i=0; i<this.grid.matrix.length; i++){
+          if(this.grid.matrix[i][item.col].value == 0)
+            column.push(this.grid.matrix[i][item.col]);
+        }
+
+        var _last = column[column.length-1]
+        //make a copy of the row
+        const newRow = this.grid.matrix[_last.row].slice(0)
+        // update the value
+        newRow[_last.col].value = this.grid.nextTurn
+        newRow[_last.col].hover = false
+        newRow[_last.col].player = this.selectedPlayer
+        // update it in the grid
+        this.$set(this.grid.matrix, _last.row, newRow)
+        this.grid.nextTurn = this.grid.nextTurn == 1 ? 2 : 1
+
+        //check winner from current cell
+        this.checkWinner(_last)
+
+        // toggle selected player if winner not found
+        this.togglePlayer()
+
       },
 
       initializePlayer () {
